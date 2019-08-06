c2fa2c115bafde3f87c92f158e0af198da2b11d9
Jens Dutzi <jens.dutzi@tf-network.de>
2019-08-06 17:59:44 +0200

    Codestyle wurde nicht für für Dateien im Hauptverzeichnis angepasst

--- config.sample.php
+++ config.sample.php
@@ -1,43 +1,44 @@
 <?php
-/**
- * DWD-Radar Video Konverter für neuthardwetter.de by Jens Dutzi - RegenRadar.php
+
+/*
+ *  RegenRadar Autoloader für neuthardwetter.de by Jens Dutzi - autoload.php
  *
- * @package    blog404de\RegenRadar
- * @author     Jens Dutzi <jens.dutzi@tf-network.de>
- * @copyright  Copyright (c) 2012-2018 Jens Dutzi (http://www.neuthardwetter.de)
- * @license    https://github.com/Blog404DE/RegenRadarVideo/blob/master/LICENSE.md
- * @version    3.1.5-stable
- * @link       https://github.com/Blog404DE/RegenRadarVideo
+ *  @package    blog404de\RegenRadar
+ *  @author     Jens Dutzi <jens.dutzi@tf-network.de>
+ *  @copyright  Copyright (c) 2012-2019 Jens Dutzi (http://www.neuthardwetter.de)
+ *  @license    https://github.com/Blog404DE/RegenRadarVideo/blob/master/LICENSE.md
+ *  @version    3.2.0-stable
+ *  @link       https://github.com/Blog404DE/RegenRadarVideo
  */
 
 // Pfade zu Konsolenprogramme:
-$converter["video"] = "/usr/local/bin/ffmpeg";
-$converter["gif"]   = "copy";
+$converter['video'] = '/usr/local/bin/ffmpeg';
+$converter['gif'] = 'copy';
 
 // Zu bearbeitende DWD-Radar-Daten:
-$config = array();
+$config = [];
 $config[] = [
-    "remoteURL"  => "https://www.dwd.de/DWD/wetter/radar/radfilm_baw_akt.gif",
-    "posterURL"  => "https://www.dwd.de/DWD/wetter/radar/rad_baw_akt.jpg",
-    "localFolder"=> "/srv/webspacepfad/tmp/radarDaten/bw",
-    "output"     => [
-        "webm"   => "/srv/webspacepfad/htdocs/img/regenradar_southwest.webm",
-        "mp4"    => "/srv/webspacepfad/htdocs/img/regenradar_southwest.mp4",
-        "gif"    => "/srv/webspacepfad/htdocs/img/regenradar_southwest.gif",
-        "poster" => "/srv/webspacepfad/htdocs/img/regenradar_southwest.jpg",
+    'remoteURL' => 'https://www.dwd.de/DWD/wetter/radar/radfilm_baw_akt.gif',
+    'posterURL' => 'https://www.dwd.de/DWD/wetter/radar/rad_baw_akt.jpg',
+    'localFolder' => '/srv/webspacepfad/tmp/radarDaten/bw',
+    'output' => [
+        'webm' => '/srv/webspacepfad/htdocs/img/regenradar_southwest.webm',
+        'mp4' => '/srv/webspacepfad/htdocs/img/regenradar_southwest.mp4',
+        'gif' => '/srv/webspacepfad/htdocs/img/regenradar_southwest.gif',
+        'poster' => '/srv/webspacepfad/htdocs/img/regenradar_southwest.jpg',
     ],
-    "forceRebuild"  => false
+    'forceRebuild' => false,
 ];
 
 $config[] = [
-    "remoteURL" => "https://www.dwd.de/DWD/wetter/radar/radfilm_brd_akt.gif",
-    "posterURL" => "https://www.dwd.de/DWD/wetter/radar/rad_brd_akt.jpg",
-    "localFolder"  => "/srv/webspacepfad/tmp/radarDaten/de",
-    "output"       => [
-        "webm" => "/srv/webspacepfad/htdocs/img/regenradar_de.webm",
-        "mp4"  => "/srv/webspacepfad/htdocs/img/regenradar_de.mp4",
-        "gif"  => "/srv/webspacepfad/htdocs/img/regenradar_de.gif",
-        "poster" => "/srv/webspacepfad/htdocs/img/regenradar_southwest.de",
+    'remoteURL' => 'https://www.dwd.de/DWD/wetter/radar/radfilm_brd_akt.gif',
+    'posterURL' => 'https://www.dwd.de/DWD/wetter/radar/rad_brd_akt.jpg',
+    'localFolder' => '/srv/webspacepfad/tmp/radarDaten/de',
+    'output' => [
+        'webm' => '/srv/webspacepfad/htdocs/img/regenradar_de.webm',
+        'mp4' => '/srv/webspacepfad/htdocs/img/regenradar_de.mp4',
+        'gif' => '/srv/webspacepfad/htdocs/img/regenradar_de.gif',
+        'poster' => '/srv/webspacepfad/htdocs/img/regenradar_southwest.de',
     ],
-    "forceRebuild"  => false
+    'forceRebuild' => false,
 ];
--- genRegenRadar.php
+++ genRegenRadar.php
@@ -1,16 +1,16 @@
 #!/usr/bin/env php
 <?php
 /**
- * DWD-Radar Video Konverter für neuthardwetter.de by Jens Dutzi - RegenRadar.php
+ * DWD-Radar Video Konverter für neuthardwetter.de by Jens Dutzi - RegenRadar.php.
  *
- * @package    blog404de\RegenRadar
  * @author     Jens Dutzi <jens.dutzi@tf-network.de>
  * @copyright  Copyright (c) 2012-2018 Jens Dutzi (http://www.neuthardwetter.de)
  * @license    https://github.com/Blog404DE/RegenRadarVideo/blob/master/LICENSE.md
+ *
  * @version    3.1.5-stable
- * @link       https://github.com/Blog404DE/RegenRadarVideo
+ *
+ * @see       https://github.com/Blog404DE/RegenRadarVideo
  */
-
 use blog404de\RegenRadar\RegenRadar;
 
 try {
@@ -19,8 +19,8 @@ try {
     $converter = [];
 
     // Root-Verzeichnis festlegen
-    if (is_readable(dirname(__FILE__) . "/config.local.php")) {
-        require_once dirname(__FILE__) . "/config.local.php";
+    if (is_readable(__DIR__ . '/config.local.php')) {
+        require_once __DIR__ . '/config.local.php';
     } else {
         throw new Exception(
             "Konfigurationsdatei 'config.local.php' existiert nicht. Zur Konfiguration lesen Sie README.md"
@@ -28,7 +28,7 @@ try {
     }
 
     // Autoloader initialisieren
-    require_once  dirname(__FILE__) . "/botLib/autoload.php";
+    require_once __DIR__ . '/botLib/autoload.php';
 
     /*
      * ================================================================================================
@@ -40,61 +40,62 @@ try {
 
     // Erzeuge Radar-Videos
     foreach ($config as $value) {
-        $header = "Beginne mit dem erzeugen des Radar-Video vom DWD Webserver: " . basename($value["remoteURL"]);
-        echo(PHP_EOL . $header . PHP_EOL . str_repeat("=", strlen($header)) . PHP_EOL);
+        $header = 'Beginne mit dem erzeugen des Radar-Video vom DWD Webserver: ' . basename($value['remoteURL']);
+        echo PHP_EOL . $header . PHP_EOL . str_repeat('=', \strlen($header)) . PHP_EOL;
 
         // Lokaler Dateiname für das Download
-        $localVideoFile = $value["localFolder"] . "/" . basename($value["remoteURL"]);
+        $localVideoFile = $value['localFolder'] . '/' . basename($value['remoteURL']);
 
         // Prüfe DWD Video nach potentiellen Updates
-        echo("Prüfe ob Update des Videos notwendig ist:" . PHP_EOL);
-        if (file_exists($localVideoFile) && !$value["forceRebuild"]) {
-            $needRebuild = $regenradarBot->network->checkDWDRadarVideoForUpdate($localVideoFile, $value["remoteURL"]);
-        } elseif ($value["forceRebuild"]) {
-            echo("-> Update des Videos ist wurde erzwungen durch die Konfigurationsdatei" . PHP_EOL);
+        echo 'Prüfe ob Update des Videos notwendig ist:' . PHP_EOL;
+        if (file_exists($localVideoFile) && !$value['forceRebuild']) {
+            $needRebuild = $regenradarBot->network->checkDWDRadarVideoForUpdate($localVideoFile, $value['remoteURL']);
+        } elseif ($value['forceRebuild']) {
+            echo '-> Update des Videos ist wurde erzwungen durch die Konfigurationsdatei' . PHP_EOL;
             $needRebuild = true;
         } else {
-            echo("-> Update des Videos ist notwendig, da noch keine lokale Datei existiert" . PHP_EOL);
+            echo '-> Update des Videos ist notwendig, da noch keine lokale Datei existiert' . PHP_EOL;
             $needRebuild = true;
         }
 
-        if (is_null($needRebuild)) {
+        if (null === $needRebuild) {
             throw new Exception(
-                "Ermitteln des Zeitpunkt des letzten Updates " .
-                "für das Radar-Videos " . basename($localVideoFile) . " fehlgeschlagen"
+                'Ermitteln des Zeitpunkt des letzten Updates ' .
+                'für das Radar-Videos ' . basename($localVideoFile) . ' fehlgeschlagen'
             );
-        } elseif ($needRebuild) {
+        }
+        if ($needRebuild) {
             // Download des Radar-Videos
-            $regenradarBot->network->downloadRadarFile($localVideoFile, $value["remoteURL"]);
+            $regenradarBot->network->downloadRadarFile($localVideoFile, $value['remoteURL']);
 
             // Erzeuge die Videos
-            foreach ($value["output"] as $filetype => $filename) {
-                if ($filetype !== "poster") {
-                    $header = "Erzeuge " . $filetype . " aus den Radar-Daten";
-                    echo(PHP_EOL . $header . PHP_EOL . str_repeat("=", strlen($header)) . PHP_EOL);
-                    if (empty($filename) || $filename === false) {
-                        echo("-> Erzeugen des " . $filetype . "-Videos in der Konfiguration deaktiviert" . PHP_EOL);
+            foreach ($value['output'] as $filetype => $filename) {
+                if ('poster' !== $filetype) {
+                    $header = 'Erzeuge ' . $filetype . ' aus den Radar-Daten';
+                    echo PHP_EOL . $header . PHP_EOL . str_repeat('=', \strlen($header)) . PHP_EOL;
+                    if (empty($filename) || false === $filename) {
+                        echo '-> Erzeugen des ' . $filetype . '-Videos in der Konfiguration deaktiviert' . PHP_EOL;
                     } else {
                         $tmpRegenAnimation = $regenradarBot->createRadarVideo($filetype, $converter, $value);
                         $regenradarBot->saveRadarVideo($tmpRegenAnimation, $filename);
                     }
                 } else {
-                    $header = "Lade Poster-Datei für das Video vom DWD Webserver";
-                    echo(PHP_EOL . $header . PHP_EOL . str_repeat("=", strlen($header)) . PHP_EOL);
+                    $header = 'Lade Poster-Datei für das Video vom DWD Webserver';
+                    echo PHP_EOL . $header . PHP_EOL . str_repeat('=', \strlen($header)) . PHP_EOL;
 
-                    if ($value["posterURL"] === false) {
-                        echo(PHP_EOL . "-> Download der Poster-Datei wird nicht benötigt" . PHP_EOL);
+                    if (false === $value['posterURL']) {
+                        echo PHP_EOL . '-> Download der Poster-Datei wird nicht benötigt' . PHP_EOL;
                     } else {
-                        $regenradarBot->network->downloadPosterFile($filename, $value["posterURL"]);
+                        $regenradarBot->network->downloadPosterFile($filename, $value['posterURL']);
                     }
                 }
             }
         }
 
-        echo(PHP_EOL . "... Auftrag ausgeführt!". PHP_EOL . PHP_EOL);
+        echo PHP_EOL . '... Auftrag ausgeführt!' . PHP_EOL . PHP_EOL;
     }
 } catch (Exception $e) {
     // Fehler-Handling
-    fwrite(STDERR, "Fataler Fehler: " . $e->getFile() . ":" . $e->getLine() . " - " . $e->getMessage());
+    fwrite(STDERR, 'Fataler Fehler: ' . $e->getFile() . ':' . $e->getLine() . ' - ' . $e->getMessage());
     exit(-1);
 }
